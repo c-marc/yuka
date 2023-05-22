571c9fb6db2e59dc1bf6c83840fb3d2eef7432a4
Marc C <mcar176@gmail.com>
2023-05-22 21:46:41 +0200

    Pick colors

---
+++ styles/global.js
@@ -0,0 +1,13 @@
+export const COLORS = {
+  darkgreen: "#57B96D",
+  green: "#5DCC71",
+  grey: "#EBEBEB",
+};
+
+export const COLORS_NS_SCALE = {
+  a: "#027D40",
+  b: "#80B52F",
+  c: "#F5DB6F",
+  d: "#EFB26F",
+  e: "#DE3C12",
+};
